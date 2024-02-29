4fc7377696e4f53b7abf58b7980caba300ed3f1a
Voytecu <voytecu@gmail.com>
2024-02-29 20:36:39 +0100

    admin login controller set to issue jwt

--- routers/LackDocktor/routes/admin-login/adminLoginController.js
+++ routers/LackDocktor/routes/admin-login/adminLoginController.js
@@ -2,16 +2,27 @@ import jwt from "jsonwebtoken";
 
 export default (req, res, next) => {
     try {
+
+        const response = {
+            jswToken: undefined,
+            auth: false,
+            message: null,
+        }
         
         if(req.body.user + req.body.password === process.env.ADMIN_PASS) {
 
             const jwtToken = jwt.sign({ site: 'LackDocktor' }, process.env.JWT_SECRET, { expiresIn: '15min' });
 
-            res.json({jwtToken});
+            response.jwtToken = jwtToken;
+            response.auth=true;
+            response.message = 'Login credentials correct, JWT issued';
+
+            res.json(response);
             return;
         }
 
-        res.json("Bad login credentials");
+        response.message = 'Wrong login credentils';
+        res.json(response);
 
     } catch (error) {
         next(error)
