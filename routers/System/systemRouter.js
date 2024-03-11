06e9f312d26e14c4f0b58c397c5b8b6adf195719
Voytecu <voytecu@gmail.com>
2024-03-11 12:47:18 +0100

    get language route added

--- routers/System/systemRouter.js
+++ routers/System/systemRouter.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import getTextLanguageController from './systemControllers/getTextLanguageController.js';
 import adminLoginController from './systemControllers/adminLoginController.js';
 import sendTextNodesController from './systemControllers/sendTextNodesController.js';
 import verifyToken from '../../middlewares/verifyToken.js';
 
 const router = express.Router();
 
+router.get('/:websiteName/languages/:language', getTextLanguageController);
 router.post('/admin-login/:site', adminLoginController);
 router.post('/send-text-nodes', verifyToken ,sendTextNodesController);
 
