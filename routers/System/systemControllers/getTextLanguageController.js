82110a30dd3ee920b33d458519f53f1fd0e480b9
Voytecu <voytecu@gmail.com>
2024-03-11 13:03:05 +0100

    get languages controller v1.0.0

---
+++ routers/System/systemControllers/getTextLanguageController.js
@@ -0,0 +1,22 @@
+import s3paths from "../../../services/AWS/S3_Bucket_FS/bucketPaths.js";
+import AWS from "aws-sdk";
+const s3 = new AWS.S3();
+
+export default async (req, res, next) => {
+
+    try {
+        console.log(req.params);
+        const { websiteName, language } = req.params;
+
+        const bucketResponse = await s3.getObject({
+            Bucket: process.env.CYCLIC_BUCKET_NAME,
+            Key: s3paths[websiteName].languages[language],
+        }).promise()
+
+        res.json(bucketResponse);
+
+    } catch (error) {
+        next(error)
+    }
+
+}
