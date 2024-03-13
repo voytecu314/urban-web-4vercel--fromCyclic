c162f4f845dd9c6bcf5be6aaed221715b607a1de
Voytecu <voytecu@gmail.com>
2024-03-13 12:54:35 +0100

    text nodes variable renamed

--- routers/System/systemControllers/sendTextNodesController.js
+++ routers/System/systemControllers/sendTextNodesController.js
@@ -7,14 +7,14 @@ export default async (req, res,next) => {
     try {
         
         const userFromTokenPayload = req.user;
-        const { root, language, textNodesJsonData } = req.body;
+        const { root, language, textNodesArray } = req.body;
 
         if(userFromTokenPayload !== root) throw new Error('Path and user don\'t match'); 
 
         const s3path = s3paths[root].languages[language.toUpperCase()];
         
         await s3.putObject({
-            Body: JSON.stringify(textNodesJsonData),
+            Body: JSON.stringify(textNodesArray),
             Bucket: process.env.CYCLIC_BUCKET_NAME,
             Key: s3path,
         }).promise()
