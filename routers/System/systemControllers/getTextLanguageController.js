c02bccb1d078e8827d662c1b9b17a6446599063e
Voytecu <voytecu@gmail.com>
2024-06-05 12:18:51 +0200

    first general changes from cyclic to vercel

--- routers/System/systemControllers/getTextLanguageController.js
+++ routers/System/systemControllers/getTextLanguageController.js
@@ -1,19 +1,10 @@
-import s3paths from "../../../services/AWS/S3_Bucket_FS/bucketPaths.js";
-import AWS from "aws-sdk";
-const s3 = new AWS.S3();
-
 export default async (req, res, next) => {
 
     try {
 
         const { websiteName, language } = req.params;
 
-        const bucketResponse = await s3.getObject({
-            Bucket: process.env.CYCLIC_BUCKET_NAME,
-            Key: s3paths[websiteName].languages[language.toUpperCase()],
-        }).promise()
-
-        res.json( JSON.parse(bucketResponse.Body.toString()) );
+        res.json( {websiteName, language} );
 
     } catch (error) {
         next(error)
--- routers/System/systemControllers/sendTextNodesController.js
+++ routers/System/systemControllers/sendTextNodesController.js
@@ -1,7 +1,3 @@
-import s3paths from "../../../services/AWS/S3_Bucket_FS/bucketPaths.js";
-import AWS from "aws-sdk";
-const s3 = new AWS.S3();
-
 export default async (req, res,next) => {
 
     try {
@@ -10,16 +6,8 @@ export default async (req, res,next) => {
         const { root, language, textNodesArray } = req.body;
 
         if(userFromTokenPayload !== root) throw new Error('Path and user don\'t match'); 
-
-        const s3path = s3paths[root].languages[language.toUpperCase()];
         
-        await s3.putObject({
-            Body: JSON.stringify(textNodesArray),
-            Bucket: process.env.CYCLIC_BUCKET_NAME,
-            Key: s3path,
-        }).promise()
-        
-        res.json({error: false, message: "Data saved !"});
+        res.json({error: false, message: "Data saved !",root, language, textNodesArray });
         
     } catch (error) {
         
