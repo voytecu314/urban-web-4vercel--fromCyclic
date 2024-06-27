40744f2fbb4885b05c9cc18968a4de7df4b5ffb1
Voytecu <voytecu@gmail.com>
2024-06-27 09:18:07 +0200

    sending nodes testing

--- api/middlewares/verifyToken.js
+++ api/middlewares/verifyToken.js
@@ -11,7 +11,7 @@ export default
             const userHash = process.env[`${user.toUpperCase()}_HASH`];
 
             const decodedPayload = jwt.verify(jwtToken, process.env.JWT_SECRET+userHash);
-            req.user = decodedPayload.user;
+            req.jwtPayload = decodedPayload;
             next();
 
           } catch(err) {
--- api/routers/System/systemControllers/sendTextNodesController.js
+++ api/routers/System/systemControllers/sendTextNodesController.js
@@ -2,12 +2,12 @@ export default async (req, res,next) => {
 
     try {
         
-        const userFromTokenPayload = req.user;
+        const jwtPayload = req.jwtPayload;
         const { user, language, textNodesArray } = req.body;
 
-        if(userFromTokenPayload !== user) throw new Error('Path and user don\'t match'); 
+        if(jwtPayload.user !== user.toLowerCase()) throw new Error('Path and user don\'t match'); 
         
-        res.json({error: false, message: "Data saved !",user, language, textNodesArray });
+        res.json({error: false, message: "Data arrived !",user, language, textNodesArray });
         
     } catch (error) {
         
