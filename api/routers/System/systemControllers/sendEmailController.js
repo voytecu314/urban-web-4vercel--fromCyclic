c97e0eb6e91b478874ca541134cfb45015966da0
Voytecu <voytecu@gmail.com>
2024-08-30 20:06:34 +0200

    sender address added

--- api/routers/System/systemControllers/sendEmailController.js
+++ api/routers/System/systemControllers/sendEmailController.js
@@ -4,7 +4,7 @@ export default async (req, res, next)=>{
 
     try {
         const info = await mailService(req.params.websiteName).sendMail({
-            from: `ME testing urban web`, // sender address
+            from: `info@urban-web.org`, // sender address
             to: "info@urban-web.org, wojtek.urbanski.web.dev@gmail.com", // list of receivers
             subject: "Your Website", // Subject line
             text: JSON.stringify(req.body), // plain text body
