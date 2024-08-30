8d8567eceee63c4e7c8a7bdc17e89bf45e732e76
Voytecu <voytecu@gmail.com>
2024-08-30 20:29:13 +0200

    leave out html from email

--- api/routers/System/systemControllers/sendEmailController.js
+++ api/routers/System/systemControllers/sendEmailController.js
@@ -8,7 +8,7 @@ export default async (req, res, next)=>{
             to: "info@urban-web.org, wojtek.urbanski.web.dev@gmail.com", // list of receivers
             subject: "Your Website", // Subject line
             text:`test: ${JSON.stringify(req.body)}`, // plain text body
-            html: `<b>TEST</b><p>test</p> <ul><li>test}</li><li>test</li><li>test</li></ul>`, // html body
+            //html: `<b>TEST</b><p>test</p> <ul><li>test}</li><li>test</li><li>test</li></ul>`, // html body
           });
         res.json(info.response);
     } catch (error) {
