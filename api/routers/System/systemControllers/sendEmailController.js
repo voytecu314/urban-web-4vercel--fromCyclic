3ea3411b460e9e0239259783c7c0ed487c6b9bb6
Voytecu <voytecu@gmail.com>
2024-08-30 20:19:22 +0200

    email textual content adjusted

--- api/routers/System/systemControllers/sendEmailController.js
+++ api/routers/System/systemControllers/sendEmailController.js
@@ -7,7 +7,7 @@ export default async (req, res, next)=>{
             from: `info@urban-web.org`, // sender address
             to: "info@urban-web.org, wojtek.urbanski.web.dev@gmail.com", // list of receivers
             subject: "Your Website", // Subject line
-            text: JSON.stringify(req.body), // plain text body
+            text:`test: ${JSON.stringify(req.body)}`, // plain text body
             html: `<b>TEST</b><p>test</p> <ul><li>test}</li><li>test</li><li>test</li></ul>`, // html body
           });
         res.json(info.response);
