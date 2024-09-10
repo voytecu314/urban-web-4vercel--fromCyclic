4e0c1ad3034527fcb4247e49ade2304cae8f841c
Voytecu <voytecu@gmail.com>
2024-09-10 14:19:24 +0200

    typo

--- api/middlewares/verifyToken.js
+++ api/middlewares/verifyToken.js
@@ -10,7 +10,7 @@ export default
             //later change to DB
             //const userHash = process.env[`${user.toUpperCase()}_HASH`];
 
-            const decodedPayload = jwt.verify(jwtToken, process.env.JWT_SECRET+user.loLowerCase());
+            const decodedPayload = jwt.verify(jwtToken, process.env.JWT_SECRET+user.toLowerCase());
             req.jwtPayload = decodedPayload;
             next();
 
