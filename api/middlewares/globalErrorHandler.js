c221e017bfe3779abe751a0cbe7a19977938ffed
Voytecu <voytecu@gmail.com>
2025-01-02 14:54:22 +0100

    global error response adjusted

--- api/middlewares/globalErrorHandler.js
+++ api/middlewares/globalErrorHandler.js
@@ -1,5 +1,5 @@
 export default (error, req, res, next) => {
     
-    res.json({error: true, message: 'Something went wrong, try again later'});
+    res.json({error: true, message: 'Something went wrong, try again later', err_message: error.message});
 
 }
