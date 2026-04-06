eb01eefe9a08a7df27520c9b9edee8605e5669eb
voytecu314 <35760560+voytecu314@users.noreply.github.com>
2026-04-06 16:24:10 +0200

    chore(server): rollback regular express cors possible issue testing: vercel authentication for preview deployments

--- api/index.js
+++ api/index.js
@@ -9,22 +9,7 @@ const app = express();
 
 const PORT = process.env.PORT || 5500;
 
-//allow all - cors issue
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "*");
-  res.setHeader("Access-Control-Allow-Headers", "*");
-
-  if (req.method === "OPTIONS") {
-    return res.status(200).end();
-  }
-
-  next();
-});
-
-// app.options("*", cors());
-// app.use( cors() );
-
+app.use( cors() );
 app.use('/',express.json());
 
 app.use('/system', systemRouter);
