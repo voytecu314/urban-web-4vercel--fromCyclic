9a569c377149234d7d41c87564fd2461d4b0c9e7
voytecu314 <35760560+voytecu314@users.noreply.github.com>
2026-05-29 22:43:27 +0200

    Back to my code after failed mini server testand setting framework to express in vercels settings under general-->deployments

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
-import express from 'express'
+import express from 'express';
+import cors from 'cors';
+import { connectToDb } from "./db/db_connection.js";
+import globalErrorHandler from './middlewares/globalErrorHandler.js';
+import systemRouter from './routers/System/systemRouter.js';
 
-const app = express()
+await connectToDb();
+const app = express();
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello World!' })
-})
+const PORT = process.env.PORT || 5500;
 
-app.listen(3000, () => {
-  console.log('Server is running')
-})
+app.use( cors() );
+app.use('/',express.json());
+
+app.use('/system', systemRouter);
+
+//Global error handle
+app.use('/', globalErrorHandler);
+
+const server = app.listen(PORT,()=>console.log('Server listens on port %s !',server.address().port));
